642b4bdc1fa1e10daf15d21a636d5e5e4e6c5877
CodeAkhil <156237911+CodeAkhil@users.noreply.github.com>
2024-05-27 17:51:44 +0530

    added

---
+++ task_movie/userregistration/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    objects = None
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profession = models.TextField(blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    mobile = models.CharField(max_length=10, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='dp', blank=True)
+
+    def __str__(self):
+        return self.user.username
